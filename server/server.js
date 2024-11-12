a6a48e8e5d241f4f0b3941430563a89b99e82562
Akash <akash.b@limitlessglobaltechnologies.com>
2024-11-12 11:40:41 +0530

    / route

--- server/server.js
+++ server/server.js
@@ -5,12 +5,11 @@ const cors = require('cors');
 const expressSession = require('express-session');
 const passport = require('./config/passport');
 
-// Import routes// server.js
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const contractRoutes = require('./routes/contractRoutes');
 
-// Create Express app
+
 const app = express();
 
 // Database connection function
@@ -27,7 +26,7 @@ const connectDb = async () => {
     }
 };
 
-// Middleware
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
@@ -38,7 +37,8 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-// Session configuration
+
+
 app.use(expressSession({
     secret: process.env.SESSION_SECRET,
     resave: false,
@@ -54,6 +54,13 @@ app.use(expressSession({
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 'success',
+        message: 'API is running'
+    });
+});
 
 // Routes
 app.use('/auth', authRoutes);
